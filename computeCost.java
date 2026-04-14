a54f8c41b84aaf1774739ef1b7c7f3e82a5255e2
Liu Jiahao <mxkq233@gmail.com>
2026-04-13 22:13:56 -0400

    just a test

--- computeCost.java
+++ computeCost.java
@@ -1,13 +1,20 @@
 import java.io.*;
 import java.util.*;
 
+public class computeCost {
 public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new FileReader("input.txt"));
         PrintWriter pw    = new PrintWriter(new FileWriter("output.txt"));
+       
+        String target = br.readLine();
+        String typo = br.readLine();
 
 
+        System.out.println("target = [" + target + "]");
+        System.out.println("typo = [" + typo + "]");
 
 
         br.close();
         pw.close();
+}
 }
