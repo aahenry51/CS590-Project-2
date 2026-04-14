942807e01feb75454bd25767f6d478d611a3cde3
Alphi Henry <aahenry51@gmail.com>
2026-04-13 21:23:25 -0400

    Init Project

---
+++ computeCost.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader("input.txt"));
+        PrintWriter pw    = new PrintWriter(new FileWriter("output.txt"));
+
+
+
+
+        br.close();
+        pw.close();
+}
